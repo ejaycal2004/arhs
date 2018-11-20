a91d8850819a4616c05dfb9a77b4497912424e2c
Elijah Calip <ecalip2022@208-engineering-4.local>
2018-11-20 13:50:32 -0800

    Initial Commit

---
+++ arhs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  arhs
+//
+//  Created by Elijah Calip on 11/20/18.
+//  Copyright © 2018 Elijah Calip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
